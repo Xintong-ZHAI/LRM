c7e93c483aa037cd0e4823c17dd34dfd87c3dba7
Xintong Zhai <zhaixt@cn056.bio.sph.umich.edu>
2023-11-16 06:47:10 -0500

    update test

---
+++ tests/testthat/test-lrm.R
@@ -0,0 +1,9 @@
+test_that("lrm works", {
+  m1 = lrm(mtcars$mpg, mtcars$wt)
+  expect_equivalent(round(lrm.estimate(m1)[,1],3),
+               round(lm(mpg~wt, data=mtcars)$coefficients,3))
+
+  m2 = lrm(CO2$uptake, CO2$conc)
+  expect_equivalent(round(lrm.estimate(m2)[,1],3),
+                    round(lm(uptake~conc, data=CO2)$coefficients,3))
+})
